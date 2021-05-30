fe7d2ffe9a576a369091c4dba09d1a21664744b0
ernestopinto <arkanoid37>
2021-05-30 12:24:47 +0100

    css rearrange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'channels', loadChildren: () => import('./components/channels/channels.module').then(m => m.ChannelsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
